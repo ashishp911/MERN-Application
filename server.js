1313168db00129b4aaa5d972168120a8c5682cf3
ashishp911 <ashish.patil35@gmail.com>
2023-02-27 16:54:26 -0500

    created server.js and added app.gets

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello world from server.")
+});
+
+app.get('/about', (req, res) => {
+    res.send("Hello world from about.")
+});
+
+app.get('/contact', (req, res) => {
+    res.send("Hello world from contact.")
+});
+
+app.get('/signin', (req, res) => {
+    res.send("Hello world from signin.")
+});
+
+app.get('/signup', (req, res) => {
+    res.send("Hello world from signup.")
+});
+
+app.listen(3000, () => {
+    console.log("server is listening at port 3000");
+})
