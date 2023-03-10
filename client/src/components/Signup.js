e07fc804e72842577e8aba2c7a67289b42b30242
ashishp911 <ashish.patil35@gmail.com>
2023-03-10 15:39:16 -0500

    signup page design p1

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,35 +1,57 @@
 import React from "react";
 import "./signup.css";
 const Signup = () => {
+  const borderStyles = {
+    border: "3px solid rgba(0, 0, 0, 0.3)",
+  };
   return (
     <>
-      <div className="container my-5">
+      <div className="container my-4">
         <h1>Registration Page</h1>
-        <form className="mt-5">
+        <form className="mt-3" id="register-form">
           <label>
             Name:
-            <input type="text" />
+            <input
+              type="text"
+              style={borderStyles}
+              placeholder="Enter your name"
+            />
           </label>
           <br />
           <label>
             Email:
-            <input type="email" />
+            <input
+              type="email"
+              style={borderStyles}
+              placeholder="Enter your email"
+            />
           </label>
           <br />
           <label>
             Phone:
-            <input type="tel" />
+            <input
+              type="tel"
+              style={borderStyles}
+              placeholder="Enter your phone-number"
+            />
           </label>
           <br />
           <label>
             Work:
-            <input type="text" />
+            <input
+              type="text"
+              style={borderStyles}
+              placeholder="Enter your profession"
+            />
           </label>
           <br />
           <label>
             Password:
             <input
               type="password"
+              style={borderStyles}
+              placeholder="Enter password"
+
               // value={password}
               // onChange={handlePasswordChange}
             />
@@ -39,6 +61,9 @@ const Signup = () => {
             Confirm Password:
             <input
               type="password"
+              style={borderStyles}
+              placeholder="Confirm password"
+
               // value={confirmPassword}
               // onChange={handleConfirmPasswordChange}
             />
