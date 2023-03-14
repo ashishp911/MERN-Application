15bcea693753bd2577790854a3e0440bd026a4c3
ashishp911 <ashish.patil35@gmail.com>
2023-03-14 17:06:09 -0400

    Home page done

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
+  const styles = {
+    container: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "90vh",
+      fontFamily: "Roboto, sans-serif",
+      textAlign: "center",
+      backgroundColor: "#bee7f8",
+    },
+    heading: {
+      fontSize: "5rem",
+      margin: 0,
+      padding: 0,
+      color: "#333",
+    },
+  };
   return (
-    <div>
-        <p>We are MERN developer (Home)</p>        
+    <div style={styles.container}>
+      <h1 style={styles.heading}>Welcome to my MERN project</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
