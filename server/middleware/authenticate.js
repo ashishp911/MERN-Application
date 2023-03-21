ff483a769dac7d023a0b14b8efc58500495c66c0
ashishp911 <ashish.patil35@gmail.com>
2023-03-20 22:55:39 -0400

    authenticate small typo solved

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -4,14 +4,14 @@ const User = require("../model/userSchema");
 const authenticate = async (req, res, next) => {
   try {
     const token = req.cookies.jwtoken;
-    const verifToken = jwt.verify(token, process.env.SECRET_KEY);
+    const verifyToken = jwt.verify(token, process.env.SECRET_KEY);
 
     const rootUser = await User.findOne({
-      _id: verifToken._id,
-      "tokens:token": token,
+      _id: verifyToken._id,
+      "tokens.token": token,
     });
     if (!rootUser) {
-      throw new Error("User npt found.");
+      throw new Error("User not found.");
     }
     req.token = token;
     req.rootUser = rootUser;
