999702bcecdc36d9da1ec2d176fb2a449fe26862
ashishp911 <ashish.patil35@gmail.com>
2023-03-22 02:16:20 -0400

    About page layout done

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,8 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import photo from "../Images/Patil_Image.JPG";
+import photo from "../Images/user.png";
+import photo2 from "../Images/user3.png";
+
+
 const About = () => {
   const navigate = useNavigate();
+
+  const [userData, setUserData] = useState();
+
   const callAboutPage = async () => {
     try {
       const response = await fetch("/about", {
@@ -15,7 +21,7 @@ const About = () => {
       });
       const data = await response.json();
       console.log(data);
-
+      setUserData(data)
       if (!response.status === 200) {
         throw new Error(response.error);
       }
@@ -24,6 +30,7 @@ const About = () => {
       navigate("/login")
     }
   };
+
   useEffect(() => {
     callAboutPage();
   }, []);
@@ -46,6 +53,16 @@ const About = () => {
       borderRadius: "1rem",
       backgroundColor: "#f5f5f5",
     },
+    box2: {
+      display: "block",
+      flexDirection: "column",
+      justifyContent: "left",
+      alignItems: "left",
+      padding: "2rem",
+      // boxShadow: "0 0 10px rgba(0, 0, 0, 0.5)",
+      borderRadius: "1rem",
+      backgroundColor: "#f5f5f5",
+    },
     heading: {
       fontSize: "2.5rem",
       marginBottom: "1rem",
@@ -61,20 +78,28 @@ const About = () => {
       <div style={styles.container}>
         <form method="GET">
           <img
-            src={photo}
+            src={photo2}
             alt="No pic"
             style={{ display: "block", margin: "auto" }}
           />
           <div style={styles.box}>
             <p style={styles.paragraph}>
-              Hi, my name is Ashish Patil. I'm a Software Developer based in
-              Buffalo, NY. I specialize in Full Stack Web Development and have
-              been working in the industry for 1 year. Lorem ipsum dolor sit
-              amet consectetur adipisicing elit. Laudantium doloribus facere
-              labore tempora eveniet maxime aut officia sunt nihil voluptates
-              quod, quaerat, atque mollitia id fugit excepturi accusamus.
-              Exercitationem illum magni eum.
+              Hi, my name is Ashish. I'm a FSE based in Buffalo, NY. 
+              Lorem ipsum dolor sitamet consectetur adipisicing elit. Laudantium doloribus facere labore tempora eveniet maxime aut officia sunt nihil voluptates
+              quod, quaerat, atque mollitia id fugit excepturi accusamus. Exercitationem illum magni eum.
+              My Contact details are as follows
             </p>
+            <br />
+          </div>
+          <div style={styles.box2}>
+            <p style={styles.paragraph}>Contact details</p>
+            <ul>
+              <li style={styles.paragraph}>Name : </li>
+              <li style={styles.paragraph}>Email Id : </li>
+              <li style={styles.paragraph}>Profession : </li>
+              <li style={styles.paragraph}>Phone Number : </li>
+
+            </ul>
           </div>
         </form>
       </div>
