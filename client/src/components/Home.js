a49b58815b107fe10f6c2ba8fbf2ebabb08452b2
ashishp911 <ashish.patil35@gmail.com>
2023-03-23 20:23:58 -0400

    Home page toggle part done

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
+  
+  const [userName, setUserName] = useState('');
+  const [show, setShow] = useState(false);
+  
+  const userHome = async () => {
+    try {
+      const response = await fetch("/getData", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
+      const data = await response.json();
+      console.log(data);
+      setUserName(data.name);
+      setShow(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    userHome();
+  }, []);
+
+
   const styles = {
     container: {
       display: "flex",
+      flexDirection: "column",
       justifyContent: "center",
       alignItems: "center",
       height: "90vh",
@@ -11,16 +39,24 @@ const Home = () => {
       textAlign: "center",
       backgroundColor: "#bee7f8",
     },
-    heading: {
+    h1_heading: {
       fontSize: "5rem",
       margin: 0,
       padding: 0,
       color: "#333",
     },
+    h2_heading: {
+      fontSize: "3rem",
+      margin: 0,
+      padding: 0,
+      color: "#333",
+    },
+
   };
   return (
     <div style={styles.container}>
-      <h1 style={styles.heading}>Welcome to my MERN project</h1>
+      <h1 style={styles.h1_heading}>Hello {userName} </h1>
+      <h2 style={styles.h2_heading}>{show ? 'Happy to see you back': 'Welcome to my MERN project'}</h2>
     </div>
   );
 };
