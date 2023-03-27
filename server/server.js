fbe8c53d2e7528014627057d7c3a93e2859eabc7
ashishp911 <ashish.patil35@gmail.com>
2023-03-26 20:16:23 -0400

    code refactoring p1

--- server/server.js
+++ server/server.js
@@ -10,31 +10,6 @@ const User = require('./model/userSchema')
 app.use(express.json());
 app.use(require('./router/auth'))
 
-// // Adding middleware
-// const middleware = (req, res, next) =>{
-//     console.log(`Hello from middleware`);
-//     // About will only load when next() function will be called, (used for authentication)
-//     next(); 
-// } 
-
-// Linking the router files 
-
-// app.get('/about', (req, res) => {
-//     res.send("Hello world from about.")
-// });
-
-// app.get('/contact', (req, res) => {
-//     res.send("Hello world from contact.")
-// });
-
-// app.get('/signin', (req, res) => {
-//     res.send("Hello world from signin.")
-// });
-
-// app.get('/signup', (req, res) => {
-//     res.send("Hello world from signup.")
-// });
-
 app.listen(PORT, () => {
     console.log(`server is listening at port ${PORT}`);
 })
